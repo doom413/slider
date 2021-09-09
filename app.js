71bae17fe19a2267bf6e23ff7c7505c05485ea8d
Serhii Tserkovnyi <dizz413@gmail.com>
2021-09-09 22:29:02 +0300

    keydown

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ upBtn.addEventListener("click", () => {
 downBtn.addEventListener("click", () => {
     changeSlide("down");
 });
+document.addEventListener('keydown', event => {
+    if (event.key === 'ArrowUp') {
+        changeSlide('up');
+    } else if (event.key === 'ArrowDown') {
+        changeSlide('down');
+    }
+});
 
 function changeSlide(direction) {
     if (direction === "up") {
