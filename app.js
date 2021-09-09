c269f595a734e09811f325776e250d6ec992303f
Serhii Tserkovnyi <dizz413@gmail.com>
2021-09-09 22:11:12 +0300

    slider

---
+++ app.js
@@ -0,0 +1,31 @@
+let downBtn = document.querySelector(".down-button");
+let upBtn = document.querySelector(".up-button");
+const sidebar = document.querySelector(".sidebar");
+const mainSlide = document.querySelector(".main-slide");
+const container = document.querySelector('.container');
+const slideCount = mainSlide.querySelectorAll('div').length;
+sidebar.style.top = `-${(slideCount-1)*100}vh`;
+let activeSlideIndex = 0;
+upBtn.addEventListener("click", () => {
+    changeSlide("up");
+});
+downBtn.addEventListener("click", () => {
+    changeSlide("down");
+});
+
+function changeSlide(direction) {
+    if (direction === "up") {
+        activeSlideIndex++;
+        if (activeSlideIndex === slideCount) {
+            activeSlideIndex = 0;
+        }
+    } else if (direction === "down") {
+        activeSlideIndex--;
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slideCount - 1;
+        }
+    }
+    const height = container.clientHeight;
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`;
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`;
+}
